d5fb1c96f9665e603b73a23d8a2a673eb7ab5262
dcrispin <dcrispin@iu.edu>
2024-02-25 20:13:41 -0500

    Homework3 Sub

--- src/main/java/dcrispin/iu/c322spring2024homework2/controllers/InventoryController.java
+++ src/main/java/dcrispin/iu/c322spring2024homework2/controllers/InventoryController.java
@@ -2,14 +2,12 @@ package dcrispin.iu.c322spring2024homework2.controllers;
 
 import dcrispin.iu.c322spring2024homework2.model.Guitar;
 import dcrispin.iu.c322spring2024homework2.repository.InventoryRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
-
+@RestController
+@CrossOrigin
 public class InventoryController {
 
     private InventoryRepository inventoryRepository;
@@ -22,6 +20,15 @@ public class InventoryController {
             return inventoryRepository.addGuitar(data);
         } catch (IOException e){
             return false;
+        }
+    }
+
+    @RequestMapping("/findAll")
+    public List<Guitar> findAll() {
+        try {
+            return inventoryRepository.findAll();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
